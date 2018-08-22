ecb8c52a72056e7fd1259e818ad0dadec6b2c131
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-22 20:25:52 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
-
+  max_value = 0
+  key_name = ""
+  name_hash.each do |name, value|
+    if value > max_value
+      key_name = name
+  end
+  key_name
 end

1930ba28f2c9cbe7700fb50913d4108f70f43c93
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-22 20:43:33 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  max_value = 0
+  max_value = 1000
       key_name = ""
 
   name_hash.collect do |name, value|
       
-    if value >= max_value
+    if value < max_value
       max_value = value
       key_name = name
     end

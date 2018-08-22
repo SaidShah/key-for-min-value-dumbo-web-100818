9cdb07c0b1c2139b8c3f7cc6ad043468f53b4edb
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-22 20:33:45 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   key_name = ""
 
   name_hash.each do |name, value|
-    if value > max_value
+    if value >= max_value
       max_value = value
       key_name = name
     end
